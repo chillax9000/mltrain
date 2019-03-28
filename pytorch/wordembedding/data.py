9096a402d7dec67aaf97bb3a513f3aea63fc850b
lpc <lpintocastaneda@gmail.com>
2019-03-28 11:49:57 +0100

    init in textdata

--- pytorch/wordembedding/data.py
+++ pytorch/wordembedding/data.py
@@ -9,25 +9,24 @@ import resources.wikipedia as wiki
 
 
 class TextData:
-    chars = string.ascii_letters
+    def __init__(self, text: str = None):
+        self.text = text if text is not None else wiki.get_cleaned_text("Cat", wiki.Lang.en).lower()
 
-    text = wiki.get_cleaned_text("Cat", wiki.Lang.en)
-    text = text.lower()
+        nltk.download("punkt")
+        nltk_text = nltk.Text(nltk.tokenize.word_tokenize(self.text))
 
-    nltk.download("punkt")
-    nltk_text = nltk.Text(nltk.tokenize.word_tokenize(text))
+        sentence_detector = nltk.data.load("tokenizers/punkt/english.pickle")
+        self.sentences = sentence_detector.tokenize(self.text)
+        self.tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in self.sentences]
 
-    sentence_detector = nltk.data.load("tokenizers/punkt/english.pickle")
-    sentences = sentence_detector.tokenize(text)
+        self.words = sorted(list(set(nltk_text.vocab().keys())))
+        self.vocab_size = len(self.words) + 1  # adding an empty token
 
-    words = sorted(list(set(nltk_text.vocab().keys())))
-    vocab_size = len(words) + 1  # adding an empty token
+        self.idx_word_dict = dict(enumerate(self.words))  # idx: word
+        self.index_empty_token = self.vocab_size - 1
+        self.idx_word_dict[self.index_empty_token] = "[EMPTY TOKEN]"
 
-    idx_word_dict = dict(enumerate(words))  # idx: word
-    index_empty_token = vocab_size - 1
-    idx_word_dict[index_empty_token] = "[EMPTY TOKEN]"
-
-    word_idx_dict = {word: idx for idx, word in idx_word_dict.items()}  # word: idx
+        self.word_idx_dict = {word: idx for idx, word in self.idx_word_dict.items()}  # word: idx
 
     def words_to_indexes(self, word_list: List):
         return [self.word_idx_dict[word] for word in word_list]
@@ -36,9 +35,7 @@ class TextData:
         return [self.idx_word_dict[index] for index in index_list]
 
     def get_sentence_indexes(self, n):
-        sentence = self.sentences[n]
-        tokens = nltk.word_tokenize(sentence)
-        return self.words_to_indexes(tokens)
+        return self.words_to_indexes(self.tokenized_sentences[n])
 
     def get_trigram_indexes(self):
         n = np.random.randint(len(self.sentences))
