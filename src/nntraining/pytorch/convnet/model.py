f01013e924fdaac3cc517a39492515c755179776
lpc <lpintocastaneda@gmail.com>
2019-05-16 23:06:46 +0200

    added batchnorm

--- src/nntraining/pytorch/convnet/model.py
+++ src/nntraining/pytorch/convnet/model.py
@@ -15,29 +15,37 @@ def convpool_n_param_out(n_chan_out, L_in, pool_ker, pool_stride, ker, n_layers=
 
 
 class ThreeLayersCNN(torch.nn.Module):
-    def __init__(self, img_side=28, n_categories=10, ker1=2, ker2=2, pool_ker=2, pool_stride=2, p_dropout=0.2,
-                 n_chan1=32, n_chan2=64, ker3=2, n_chan3=128, pool_fn=torch.nn.MaxPool2d, n_linear=128,
+    def __init__(self, img_side=28, ker1=2, ker2=2, pool_ker=2, pool_stride=2, p_dropout=0.2, n_chan1=32,
+                 n_chan2=64,
+                 ker3=2, n_chan3=128, pool_fn=torch.nn.MaxPool2d, activ_fn=torch.nn.ReLU, n_linear=128,
                  device=default_device):
         super().__init__()
         self.convnet = torch.nn.Sequential(
             torch.nn.Conv2d(1, n_chan1, ker1),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan1),
+            activ_fn(),
             torch.nn.Conv2d(n_chan1, n_chan1, ker1),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan1),
+            activ_fn(),
             pool_fn(pool_ker, pool_stride),
 
             torch.nn.Conv2d(n_chan1, n_chan2, ker2),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan2),
+            activ_fn(),
             torch.nn.Conv2d(n_chan2, n_chan2, ker2),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan2),
+            activ_fn(),
             pool_fn(pool_ker, pool_stride),
 
             torch.nn.Conv2d(n_chan2, n_chan3, ker3),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan3),
+            activ_fn(),
             torch.nn.Conv2d(n_chan3, n_chan3, ker3),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan3),
+            activ_fn(),
             torch.nn.Conv2d(n_chan3, n_chan3, ker3),
-            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(n_chan3),
+            activ_fn(),
             pool_fn(pool_ker, pool_stride)
         ).to(device)
         L_l1_out = convpool_L_out(img_side, pool_ker, pool_stride, ker1, n_layers=2)
@@ -46,12 +54,11 @@ class ThreeLayersCNN(torch.nn.Module):
 
         self.layer_out = torch.nn.Sequential(
             torch.nn.Linear(self.convpool_n_param_out, n_linear),
-            torch.nn.ReLU(),
+            activ_fn(),
             torch.nn.Dropout2d(p=p_dropout),
             torch.nn.Linear(n_linear, n_linear),
-            torch.nn.ReLU(),
-            torch.nn.Dropout2d(p=p_dropout),
-            torch.nn.Linear(n_linear, n_categories).to(device)
+            activ_fn(),
+            torch.nn.Linear(n_linear, 10)
         ).to(device)
 
     def forward(self, x):
