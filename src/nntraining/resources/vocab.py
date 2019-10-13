97c4f92440dae24efbb9e5365c13cd0bd3ef718a
lpc <lpintocastaneda@gmail.com>
2019-10-13 13:31:40 +0200

    adding inflections to extracted vocab

--- src/nntraining/resources/vocab.py
+++ src/nntraining/resources/vocab.py
@@ -7,31 +7,54 @@ import os
 import xml.etree.ElementTree
 import time
 import simpleclock
+from collections import defaultdict
 
 file_path = os.path.join(os.path.dirname(__file__), ".nocommit", "Morphalou-2.0.xml")
 
 clock = simpleclock.Clock.started()
 
-print("parsing xml")
 tree = xml.etree.ElementTree.parse(file_path)
 stop = time.perf_counter_ns()
-clock.elapsed_since_start.print("end")
+clock.elapsed_since_start.print("parsing took")
 
-print("processing")
 root = tree.getroot()
-words = {}
-for child in root:
-    if child.tag == "lexicalEntry":
-        lemmatized_form = child.find("formSet").find("lemmatizedForm")
-        ortography = lemmatized_form.find("orthography").text
-        grammatical_cat = lemmatized_form.find("grammaticalCategory").text
-        words[ortography] = grammatical_cat
-clock.elapsed_since_last_call.print("end")
+words = defaultdict(lambda: set())
+for lexical_entry in root.findall("lexicalEntry"):
+    formset = lexical_entry.find("formSet")
+    # base
+    lemmatized_form = formset.find("lemmatizedForm")
+    orthography = lemmatized_form.find("orthography").text
+    grammatical_cat = lemmatized_form.find("grammaticalCategory").text
+    words[orthography].add(grammatical_cat)
+    # inflections
+    for inflected_form in formset.iterfind("inflectedForm"):
+        orthography = inflected_form.find("orthography").text
+        words[orthography].add(grammatical_cat)
+clock.elapsed_since_last_call.print("processing took")
 
-grammatical_cats = set(words.values())
+grammatical_cats = set()
+for cats in words.values():
+    grammatical_cats.update(cats)
 print(f"{len(words)} words saved")
 print(f"grammatical categories found: {sorted(grammatical_cats)}")
 
 with open(os.path.join(os.path.dirname(__file__), "vocab-fr.txt"), "w") as f:
     for word in words:
         f.write(f"{word}\n")
+clock.elapsed_since_last_call.print("writing took")
+
+"""
+ <lexicalEntry id="abandonner_1">
+                <formSet>
+                        <lemmatizedForm>
+                                <orthography>abandonner</orthography>
+                                <grammaticalCategory>verb</grammaticalCategory>
+                        </lemmatizedForm>
+                        <inflectedForm>
+                                <orthography>abandonna</orthography>
+                                <grammaticalNumber>singular</grammaticalNumber>
+                                <grammaticalMood>indicative</grammaticalMood>
+                                <grammaticalTense>simplePast</grammaticalTense>
+                                <grammaticalPerson>thirdPerson</grammaticalPerson>
+                        </inflectedForm>
+"""
