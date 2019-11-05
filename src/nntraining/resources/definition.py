3806f2c1d7a09942e0b455bcbb38022cdbbfedfd
lpc <lpintocastaneda@gmail.com>
2019-11-05 11:44:17 +0100

    improved refined + read refined

--- src/nntraining/resources/definition.py
+++ src/nntraining/resources/definition.py
@@ -1,17 +1,23 @@
 import os
 import re
-import simpleclock
+from pprint import pprint
 
-xml_path = os.path.join(".nocommit", "frwiktionary-20191020-pages-articles.xml")
-raw_path = os.path.join(".nocommit", "raw")
-pruned_path = os.path.join(".nocommit", "pruned")
-refined_path = os.path.join(".nocommit", "refined")
+import simpleclock
+import json
+import collections
+
+folder_path = os.path.join(os.path.dirname(__file__), ".nocommit")
+xml_path = os.path.join(folder_path, "frwiktionary-20191020-pages-articles.xml")
+raw_path = os.path.join(folder_path, "raw")
+pruned_path = os.path.join(folder_path, "pruned")
+refined_path = os.path.join(folder_path, "refined")
 
 MAX_C = 20_000_000
 
 RE_XML_TO_RAW = re.compile(r"('''|===|# )")
-RE_FIND_WORD = re.compile(r"^'''[^']*'''")
+RE_FIND_WORD = re.compile(r"^'''([^']*)'''")
 RE_FIND_LINK = re.compile(r"\[\[(?P<link>[^|\]]+)\|?(?P<text>[^|\]]+)?\]\]")
+RE_FIND_GRAMMAR = re.compile(r"^=== {{S\|([^|}]*)\|fr}} ===")
 RE_FIND_CAT = re.compile(r"\{\{(.+?)\}\}")
 RE_FIND_FICHIER = re.compile(r"\[\[(Fichier|File|Image):.*?\]\]")
 
@@ -90,26 +96,64 @@ def prune_raw(input_path, output_path, max_lines_w=MAX_C):
 
 def refine_pruned(input_path, output_path, max_lines_w=MAX_C):
     with open(input_path, "r") as f_r, open(output_path, "w") as f_w:
-        c = 0
-        # missed = 0
+        _count = 0
+        missed = 0
+        writing_defs = False
+        word = None
+        grammatical_cat = None
+        gram_errors = []
+        word_errors = []
+        def_errors = []
+        unknown_errors = []
         for line in f_r:
-            if line.startswith("#"):
-                line_orig = line
+            if line.startswith("#"):  # definition
+                if not writing_defs:
+                    f_w.write(f"> {word} || {grammatical_cat}\n")
+                    writing_defs = True
                 line = line[1:].strip()
                 line, cats = extract_and_delete_categories(line)
                 line, links = extract_and_replace_links(line)
                 line = delete_fichiers(line)
-                # if "[[" in line or "{{" in line:
-                #     if missed < 100:
-                #         print(line)
-                #     missed += 1
-                line = "# " + line.strip() + " " + str({"links": links, "categories": cats}) + "\n"
-            if line.startswith("'''"):
-                line = process_def_line(line)
-            f_w.write(line)
-            c += 1
-            if c > max_lines_w:
+                if "[[" in line or "{{" in line:
+                    def_errors.append(line)
+                f_w.write(json.dumps({"definition": line.strip(), "links": links, "categories": cats}) + "\n")
+            else:
+                if writing_defs:
+                    writing_defs = False
+                line = line.strip()
+                if line.startswith("'''"):
+                    word_match = RE_FIND_WORD.match(line)
+                    word = word_match.group(1) if word_match else None
+                    if not word:
+                        word_errors.append(line)
+                elif line.startswith("==="):
+                    gram_match = RE_FIND_GRAMMAR.match(line)
+                    grammatical_cat = gram_match.group(1) if gram_match else None
+                    if not grammatical_cat:
+                        gram_errors.append(line)
+                else:
+                    unknown_errors.append(line)
+            _count += 1
+            if _count > max_lines_w:
                 break
+        print("missed:", missed)
+
+
+def read_refined(input_path):
+    def read_header(stripped_line):
+        return tuple(map(str.strip, stripped_line[2:].split("||")))
+
+    words = collections.defaultdict(lambda: [])
+    with open(input_path) as f_r:
+        for line in f_r:
+            line = line.strip()
+            if line.startswith(">"):
+                word, gram_type = read_header(line)
+            else:
+                if word != "None" and gram_type != "None":
+                    definition = json.loads(line)
+                    words[(word, gram_type)].append(definition)
+    return words
 
 
 if __name__ == "__main__":
