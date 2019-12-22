9e86142f9295c16c940ecbd5b9c380be398bb45e
lpc <lpintocastaneda@gmail.com>
2019-12-23 00:35:46 +0100

    reworking wiktionary dump process

--- src/nntraining/resources/definition.py
+++ src/nntraining/resources/definition.py
@@ -5,9 +5,10 @@ from pprint import pprint
 import simpleclock
 import json
 import collections
+import xml.etree.ElementTree
 
 folder_path = os.path.join(os.path.dirname(__file__), ".nocommit")
-xml_path = os.path.join(folder_path, "frwiktionary-20191020-pages-articles.xml")
+xml_path = os.path.join(folder_path, "frwiktionary-20191201-pages-articles.xml")
 raw_path = os.path.join(folder_path, "raw")
 pruned_path = os.path.join(folder_path, "pruned")
 refined_path = os.path.join(folder_path, "refined")
@@ -73,10 +74,75 @@ def process_def_line(line):
 def xml_to_raw(input_path, output_path, max_lines_w=MAX_C):
     with open(input_path, "r") as f_r, open(output_path, "w") as f_w:
         c = 0
+        RE_TITLE = re.compile(r"<title>(.*)</title>")
+        titles = 0
         for line in f_r:
+            title_match = RE_TITLE.search(line)
+            if title_match:
+                f_w.write(line)
+                titles += 1
+                if titles % 100 == 0 and titles > 0:
+                    print(f"{titles} processed")
             if RE_XML_TO_RAW.match(line):
                 f_w.write(line)
                 c += 1
+            if c > max_lines_w:
+                break
+
+
+def study(input_path, max_lines_w=MAX_C):
+    RE_SUBSECTION = re.compile(r"(?<!=)=== {{(.*)}} ===")
+    sections = []
+    c = 0
+    with open(input_path, "r") as f_r:
+        for line in f_r:
+            matches = RE_SUBSECTION.search(line)
+            if matches:
+                sections.append(matches.group(1))
+            c += 1
+            if c >= max_lines_w:
+                break
+    counter = collections.Counter(sections)
+    print(len(counter))
+    pprint(counter.most_common(100))
+
+
+def parse_xml(input_path, output_path, max_lines_w=MAX_C):
+    RE_OK = re.compile(r"\|fr(\||$)")
+
+    def valid_subsection(s):
+        return RE_OK.search(s) is not None
+
+    RE_TITLE = re.compile(r"<title>(.*)</title>")
+    RE_TXT_END = re.compile(r"</text>")
+    RE_SECTION = re.compile(r"(?<!=)== {{(.*)}} ==")
+    RE_SUBSECTION = re.compile(r"(?<!=)=== {{(.*)}} ===")
+    WHATEV_SECTION = re.compile(r"== {{(.*)}} ==")
+    with open(input_path, "r") as f_r, open(output_path, "w") as f_w:
+        c = 0
+        titles = 0
+        lang_fr = False
+        ok_subsection = False
+        for line in f_r:
+            title_match = RE_TITLE.search(line)
+            if title_match:
+                f_w.write(line)
+                titles += 1
+                if titles % 100000 == 0 and titles > 0:
+                    print(f"{titles} titles")
+            whatev_section_match = WHATEV_SECTION.search(line)
+            section_match = RE_SECTION.search(line) if whatev_section_match else False
+            subsection_match = RE_SUBSECTION.search(line) if whatev_section_match else False
+            endline_match = RE_TXT_END.search(line)
+            if section_match:
+                lang_fr = section_match.group(1) == "langue|fr"
+            ok_subsection = ((ok_subsection and not (whatev_section_match or endline_match)) or
+                             subsection_match and valid_subsection(subsection_match.group(1)))
+            if lang_fr and ok_subsection:
+                f_w.write(line)
+                c += 1
+                if c % 100000 == 0 and c > 0:
+                    print(c, "rows")
             if c > max_lines_w:
                 break
 
@@ -160,9 +226,10 @@ def read_refined(input_path, max_words=None):
 
 if __name__ == "__main__":
     clock = simpleclock.Clock.started()
-    xml_to_raw(xml_path, raw_path)
+    study(raw_path, 1_000_000)
+    # parse_xml(xml_path, raw_path, 1_000_000)
     clock.elapsed_since_last_call.print("xml to raw")
-    prune_raw(raw_path, pruned_path)
-    clock.elapsed_since_last_call.print("pruned")
-    refine_pruned(pruned_path, refined_path)
-    clock.elapsed_since_last_call.print("refined")
+    # prune_raw(raw_path, pruned_path)
+    # clock.elapsed_since_last_call.print("pruned")
+    # refine_pruned(pruned_path, refined_path)
+    # clock.elapsed_since_last_call.print("refined")
