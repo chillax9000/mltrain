a4e4e8a1857dbe04230ea7cc048580084267f31c
lpc <lpintocastaneda@gmail.com>
2020-04-19 23:46:45 +0200

    test gym

---
+++ src/nntraining/rl/gymcart_ql.py
@@ -0,0 +1,65 @@
+import time
+import collections
+import gym
+import sklearn
+import numpy as np
+import operator
+import pprint
+
+
+def get_state(obs, mins, maxs):
+    return tuple((10 * ((obs - mins) / (maxs - mins))).astype(int))
+
+
+def update_minmax(obs, mins, maxs):
+    beef = np.concatenate((obs, mins, maxs)).reshape(-1, 4)
+    return beef.min(axis=0), beef.max(axis=0)
+
+
+def policy(state, values, action_space):
+    choices = [(action, values[(state, action)]) for action in action_space]
+    return sorted(choices, key=operator.itemgetter(1), reverse=True)[0][0]
+
+
+alpha = .1
+gamma = .1
+values = collections.defaultdict(lambda: 0)
+
+history = []
+
+mins = np.array([0, 0, 0, 0])
+maxs = np.array([1, 1, 1, 1])
+
+lengths = []
+env = gym.make('CartPole-v0')
+for i_episode in range(100):
+    # init
+    observation = env.reset()
+    state = get_state(observation, *update_minmax(observation, mins, maxs))
+    for t in range(100):
+        # env.render()
+        # time.sleep(.01)
+
+        # compute
+        action = policy(state, values, [0, 1])
+        observation, reward, done, info = env.step(action)
+
+        # update value
+        next_state = get_state(observation, *update_minmax(observation, mins, maxs))
+        next_values = [values[(next_state, action)] for action in [0, 1]]
+        values[(state, action)] += \
+            alpha * (reward + gamma * max(next_values) - values[state])
+        state = next_state
+
+        if done:
+            lengths.append(t+1)
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
+
+size = 10
+buckets = [size * (l // size) for l in lengths]
+print()
+print(collections.Counter(buckets).most_common(20))
+pprint.pprint(list(sorted(values.items(), key=operator.itemgetter(1),
+                          reverse=True))[:20])
